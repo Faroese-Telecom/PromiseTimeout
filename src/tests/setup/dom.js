c9ef67b2f3c38cccdb9f85cc26066b6f51151fd1
Jóhann Østerø <jost@ft.local>
2019-05-21 09:52:28 +0100

    Cleaned up the development side

--- src/tests/setup/dom.js
+++
@@ -1,9 +0,0 @@
-import "jsdom-global/register";
-
-global.requestAnimationFrame = callback => {
-  setTimeout(callback, 0);
-};
-
-global.cancelAnimationFrame = callback => {
-  setTimeout(callback, 0);
-};
